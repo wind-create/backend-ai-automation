272758613ba2622fd885dcfab9fb5b0e7135a44c
wind-create <nardowilli@gmail.com>
2026-02-14 11:21:58 +0700

    first commit

---
+++ src/main/java/com/bot/HumanLikeAutomationBot/HumanLikeAutomationBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.HumanLikeAutomationBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanLikeAutomationBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanLikeAutomationBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bot/HumanLikeAutomationBot/HumanLikeAutomationBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.HumanLikeAutomationBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanLikeAutomationBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
